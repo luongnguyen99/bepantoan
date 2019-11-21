2319b9d89d03bf07c8272ca8fa4d6a8ab5444d0a
Nguyễn Đại Lượng <35337397+luongnguyen99@users.noreply.github.com>
2019-11-21 08:49:54 +0700

    upload

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -53,6 +53,10 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
+                    <li class="{{activeNav('products')}}"><a href="{{route('admin.products.index')}}"><i class="fa fa-circle-o"></i>
+                                    Danh sách sản phẩm</a></li>
+                    <li class="{{activeNav('products','add')}}"><a href="{{route('admin.products.index')}}"><i class="fa fa-circle-o"></i>
+                                            Danh sách sản phẩm</a></li>
                     <li class="{{activeNav('properties')}}"><a href="{{route('admin.properties.index')}}"><i
                                 class="fa fa-circle-o"></i> Thuộc tính</a></li>
                     <li class="{{activeNav('categories')}}"><a href="{{route('admin.categories.index')}}"><i
