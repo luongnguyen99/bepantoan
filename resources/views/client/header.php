4aea0296b84d0d3c030fa8d82483577bf8ea25f0
Luong <luongnd2286@gmail.com>
2019-11-20 19:51:33 -0700

    first commit host

---
+++ resources/views/client/header.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/font-akr.css">
+	<link rel="stylesheet" href="css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="css/helper.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="header-pc hidden-xs hidden-sm">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2 col-xs-12 col-sm-12">
+						<div class="logo">
+							<a href="#"><img src="img/logo-bepantoan.png" alt=""></a>
+						</div>
+					</div>
+					<div class="col-md-3 col-xs-12 col-sm-12">
+						<div class="form-search">
+							<form>
+								<input type="text" name="" class="form-control" placeholder="Nhập từ khóa tìm kiếm">
+								<button type="button" class="btn">Tìm kiếm</button>
+								<ul class="resuiltSearch ul-menu-muiten search-suggest">
+								    <li class="page">Tìm trong <a href="#" target="_blank">Lò nướng Hafele</a></li>
+								    <li class="page">Tìm trong <a href="#" target="_blank">Tủ lạnh Hafele</a></li>
+								    <li class="page">Tìm trong <a href="#" target="_blank">Tủ rượu Hafele</a></li>
+								    <li>
+                                        <a href="#">
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img src="https://beptot.vn/Data/ResizeImage/images/7636_bep_gas_am_eurosun_eu_ga287x100x100x4.jpg" class="media-object thumb">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading name-prd">Bếp Gas Âm Eurosun EU-GA287</h4>
+                                                    <p class="pri-item ss-name">2,760,000<sup>₫</sup></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img src="https://beptot.vn/Data/ResizeImage/images/7636_bep_gas_am_eurosun_eu_ga287x100x100x4.jpg" class="media-object thumb">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading name-prd">Bếp Gas Âm Eurosun EU-GA287</h4>
+                                                    <p class="pri-item ss-name">2,760,000<sup>₫</sup></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+								</ul>
+
+							</form>
+						</div>
+					</div>
+					<div class="col-md-7 col-xs-12 col-sm-12">
+						<div class="quick-menu hidden-xs">
+					        <a href="#"><i class="pe-7s-shopbag"></i>Sản phẩm</a>
+					        
+					        <a href="#" rel=""><i class="pe-7s-news-paper"></i>Tin tức</a>
+					        
+					        <a href="#" rel=""><i class="pe-7s-tools"></i>Dịch vụ</a>
+					        
+					        <a class="km" href="#" target="_blank" rel=""><i class="pe-7s-gift"></i>Khuyến mãi</a>
+					        <a class="hotline" href="#" target="_blank" rel=""><i class="pe-7s-call"></i>0986.083.083</a>
+					    </div>
+					    <div class="quick-cart">
+					        <a href="#" target="_blank" rel="">
+					            <img alt="BẾP TỐT" src="img/cart_bg.png"><span class="num">0</span>
+					        </a>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="header-mobile hidden-md hidden-lg">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<div class="menu-site">
+			                <button class="btn btn-show-menu hidden-md hidden-lg"><i class="fa fa-bars"></i></button>
+			                <div class="menu-box">
+			                    <div class="bg-menu hidden-md hidden-lg"></div>
+			                    <ul class="main-menu">
+			                    	<span class="logo-menu">
+			                    		<img src="img/logo-bepantoan.png" alt="">
+			                    	</span>
+			                        <li class="ng-scope  drop-icon re-icon">
+					                    <a href="#"><span>
+					                        <img src="https://beptot.vn/Data/ResizeImage/files/page/bep_tux100x100x4.png" alt="Bếp từ"></span>Bếp từ
+					                    </a>
+					                </li>
+			                        <li class="ng-scope  drop-icon re-icon">
+					                    <a href="#"><span>
+					                        <img src="https://beptot.vn/Data/ResizeImage/files/page/bep_tux100x100x4.png" alt="Bếp từ"></span>Bếp từ
+					                    </a>
+					                </li>
+					                <li class="ng-scope  drop-icon re-icon menu-item-has-children">
+					                    <a href="#"><span>
+					                        <img src="https://beptot.vn/Data/ResizeImage/files/page/bep_tux100x100x4.png" alt="Bếp từ"></span>Bếp từ
+					                    </a>
+					                    <ul class="sub-menu">
+					                        <li class="ng-scope ng-has-child1"><a href="#">Máy sấy chén bát âm tủ</a> </li>
+					                        <li class="ng-scope ng-has-child1"><a href="#">Máy sấy chén bát âm tủ</a> </li>
+					                    </ul>
+					                </li>
+					                <ul class="mobile-support">
+								        <li><i class="pe-7s-news-paper"></i><a href="#c">Tin tức</a></li>
+								        <li><i class="pe-7s-tools"></i><a href="#">Dịch vụ</a></li>
+								        <li><i class="pe-7s-gift"></i><a href="#">Khuyến mãi</a></li>
+								        <li>
+								            <i class="pe-7s-call"></i>
+								            Hotline <b>0986.083.083</b> (24h/7) 
+								        </li>
+    								</ul>
+
+			                    </ul>
+ 			                    <!--<button class="btn btn-hide-menu hidden-md hidden-lg"><i class="fa fa-times"></i></button> -->
+			                </div>
+				        </div>
+					</div>
+					<div class="col-xs-6 col-sm-8">
+						<div class="logo">
+							<a href="#"><img src="img/logo-bepantoan.png" alt=""></a>
+						</div>
+						
+					</div>
+					<div class="col-xs-3 col-sm-2">
+						<div class="quick-cart">
+					        <a href="#" target="_blank" rel="">
+					            <img alt="BẾP TỐT" src="img/cart_bg.png"><span class="num">0</span>
+					        </a>
+					    </div>
+					</div>
+					<div class="col-xs-12 col-sm-12">
+						<div class="form-search">
+							<form>
+								<input type="text" name="" class="form-control" placeholder="Nhập từ khóa tìm kiếm">
+								<button type="button" class="btn">Tìm kiếm</button>
+								<ul class="resuiltSearch ul-menu-muiten search-suggest">
+								    <li class="page">Tìm trong <a href="#" target="_blank">Lò nướng Hafele</a></li>
+								    <li class="page">Tìm trong <a href="#" target="_blank">Tủ lạnh Hafele</a></li>
+								    <li class="page">Tìm trong <a href="#" target="_blank">Tủ rượu Hafele</a></li>
+								    <li>
+                                        <a href="#">
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img src="https://beptot.vn/Data/ResizeImage/images/7636_bep_gas_am_eurosun_eu_ga287x100x100x4.jpg" class="media-object thumb">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading name-prd">Bếp Gas Âm Eurosun EU-GA287</h4>
+                                                    <p class="pri-item ss-name">2,760,000<sup>₫</sup></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img src="https://beptot.vn/Data/ResizeImage/images/7636_bep_gas_am_eurosun_eu_ga287x100x100x4.jpg" class="media-object thumb">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading name-prd">Bếp Gas Âm Eurosun EU-GA287</h4>
+                                                    <p class="pri-item ss-name">2,760,000<sup>₫</sup></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+								</ul>
+
+							</form>
+						</div>
+						
+					</div>
+					<div class="col-xs-12 col-sm-12">
+						<div class="btn-group-cate hidden-lg hidden-md">
+					        <button type="button" class="btn-category dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+					            Danh mục sản phẩm <span class="caret"></span>
+					        </button>
+					        <ul class="dropdown-menu sub-categories-mobile" role="menu">
+					        	<?php foreach(range(1, 8) as $number ): ?>
+					            <li>
+					            	<a href="#" rel=""><span>
+					                <img src="https://beptot.vn/Data/ResizeImage/files/page/bep_tux100x100x4.png" alt="Bếp từ"></span>Bếp từ</a> 
+					            </li>
+					            <?php endforeach; ?>
+					        </ul>
+					    </div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		
+	</header><!-- /header -->
